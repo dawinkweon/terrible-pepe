cc9317e331b8c6c4036d413b389cab0430ab7ed9
violet-bl-cl <skyrheem@gmail.com>
2024-08-03 18:12:50 +1200

    update audio player

--- src/background.ts
+++ src/background.ts
@@ -1,7 +1,2 @@
-import {runtime} from "webextension-polyfill"
-import { playAudioAtURL } from "./components/AudioSource";
-//playAudioAtURL(runtime.getURL("./assets/sfx_pop_appear.mp3"), false);
-
-
-console.log('Hello Background PEPE');
+console.log('Hello Background');
 export {}
--- src/components/ToggleSwitch/ToggleSwitch.tsx
+++ src/components/ToggleSwitch/ToggleSwitch.tsx
@@ -18,7 +18,9 @@ const ToggleSwitch: React.FC<ToggleSwitchProps> = ({
   const [isToggled, setIsToggled] = useState<boolean>(isOn);
 
   const handleToggle = () => {
-    const newIsToggled = !isToggled;
+    
+   
+    const newIsToggled = !isToggled;    
     setIsToggled(newIsToggled);
     onToggle(newIsToggled);
   };
