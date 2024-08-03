a46a73811d5edbd9561b2157cacb1ac53c648667
SeongrokShin <ssr7324@gmail.com>
2024-08-03 18:06:45 +1200

    .

--- src/content.ts
+++ src/content.ts
@@ -3,24 +3,22 @@ import { runtime } from "webextension-polyfill";
 console.log("Hello Content");
 export {};
 
-
 function changeImages(imgs: HTMLCollectionOf<HTMLImageElement>) {
-    for (let i = 0; i < imgs.length; i++) {
-        let r = Math.floor(Math.random() * 14);
-        let file = `images/pepe${r + 1}.png`;
-        let url = runtime.getURL(file);
-        
-        const img = imgs.item(i);
-        img?.setAttribute("currentSrc", url);
-        
-        img!.src = url;
-    }
+  for (let i = 0; i < imgs.length; i++) {
+    let r = Math.floor(Math.random() * 14);
+    let file = `images/pepe${r + 1}.png`;
+    let url = runtime.getURL(file);
+
+    const img = imgs.item(i);
+    img?.setAttribute("currentSrc", url);
+
+    img!.src = url;
+  }
 }
 
 const init = () => {
-  OverlayImage().on();
   changeImages(document.getElementsByTagName("img"));
+  OverlayImage().on();
 };
-
 
 init();
