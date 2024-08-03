89030e50eca7e78036da52acfff7b878c0287916
AliceHeeyeon <aliceheeyeonkim@gmail.com>
2024-08-03 17:40:04 +1200

    finally change all images to pepe

--- src/components/changeImagetoPepe.tsx
+++ src/components/changeImagetoPepe.tsx
@@ -1,17 +1,15 @@
 import React from 'react'
 
-type document = 
+//const changeImagetoPepe = (doc: document) => {
 
-const changeImagetoPepe = (doc: document) => {
+    // document.getElementsByTagName('img').map((img) => {
+    //     console.log(img.src)
 
-    document.getElementsByTagName('img').map((img) => {
-        console.log(img.src)
+    //     let r = Math.floor(Math.random() * 15)
+    //     let file = `images/pepe${r}.png`
+    //     let url = chrome.extension.getURL(file)
+    //     img.src = url 
+    // })
+//}
 
-        let r = Math.floor(Math.random() * 15)
-        let file = `images/pepe${r}.png`
-        let url = chrome.extension.getURL(file)
-        img.src = url 
-    })
-}
-
-export default changeImagetoPepe
+//export default changeImagetoPepe
--- src/content.ts
+++ src/content.ts
@@ -1,8 +1,24 @@
-import React from 'react'
+import { runtime } from "webextension-polyfill";
 
-const content = () => {
+export {};
 
+let imgs = document.getElementsByTagName('img');
+
+function changeImages() {
+    for (let i = 0; i < imgs.length; i++) {
+        let r = Math.floor(Math.random() * 14 )
+        let file = `images/pepe${r + 1}.png`
+        let url = runtime.getURL(file)
+        
+        const img = imgs.item(i)
+        img?.setAttribute("currentSrc", url)
+        
+        img!.src = url
+    }   
 }
 
-export default content
+changeImages()
+
+
+
 
