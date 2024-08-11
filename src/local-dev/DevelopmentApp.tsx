ff89b437281e9277da5700eea4eabb70b0ee4c73
dawinkweon <windarena@gmail.com>
2024-08-11 21:03:36 +1200

    simplify index page

--- src/local-dev/DevelopmentApp.tsx
+++ src/local-dev/DevelopmentApp.tsx
@@ -1,53 +1,41 @@
 import React from "react";
-import { createBrowserRouter, Link, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import GrindingModeDialog from "../components/GrindingModeDialog/GrindingModeDialog";
 import Popup from "../views/Popup/App";
 import Options from "../views/Options/App";
 
 import EyeSaverDialog from "../components/EyeSaverDialog/EyeSaverDialog";
+import { IndexPage } from "./IndexPage";
+
+export type ComponentInfo = {
+  component: React.JSX.Element;
+  alias?: (string | undefined);
+};
+
+// Update this when adding a new component
+const COMPONENTS : ComponentInfo[] = [
+  { component: <GrindingModeDialog /> },
+  { component: <EyeSaverDialog /> },
+  { component: <Popup />, alias: "Popup" },
+  { component: <Options />, alias: "Options" },
+];
 
 const DevelopmentApp = () => {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: (
-        <div>
-          <ul>
-            <li>
-              <Link to="/grinding">GrindingModeDialog</Link>
-            </li>
-            <li>
-              <Link to="/eyesaver">EyeSaverDialog</Link>
-            </li>
-            <li>
-              <Link to="/popup">Popup</Link>
-            </li>
-            <li>
-              <Link to="/options">Options</Link>
-            </li>
-          </ul>
-        </div>
-      ),
-    },
-    {
-        path: "/grinding",
-        element: <GrindingModeDialog />
-    },
-    {
-        path: "/eyesaver",
-        element: <EyeSaverDialog />
-    },
-    {
-        path: "/popup",
-        element: <Popup />
-    },
-    {
-        path: "/options",
-        element: <Options />
-    }
-  ]);
+  const indexRoute = {
+    path: "/",
+    element: <IndexPage components={COMPONENTS} />,
+  };
+
+  const router = createBrowserRouter([indexRoute, ...mapToRoutes(COMPONENTS)]);
 
   return <RouterProvider router={router} />;
+};
+
+const mapToRoutes = (components: ComponentInfo[]) => {
+  // e.g. Generates a path: /Options for Options component
+  return components.map(({component, alias}) => {
+    return { path: `/${alias ?? component.type.name}`, element: component };
+  });
 };
 
 export default DevelopmentApp;
